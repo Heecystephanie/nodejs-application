673c827d0adb839fa4f25bb8485306973a10cefb
Heecystephanie <89740012+Heecystephanie@users.noreply.github.com>
2023-04-15 17:34:16 -0600

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://54.91.201.215:9000/',
+  serverUrl: 'http://54.176.114.164:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsLandmarkTechnologies',
-	    'sonar.login': '75d367d10d86227c8be4174dfc7deb0328c81291',
+	    'sonar.login': '567a6d4792df3c6b41cb4d302ea12565ca27b754',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
